2d5096fd3ef11edb4f3e0a226f8a5ec5c64e932e
delarobia <delarobiaxana@gmail.com>
2024-05-29 09:19:32 +0200

    mise en page

--- src/assets/screen/AccueilScreen.ts
+++ src/assets/screen/AccueilScreen.ts
@@ -3,45 +3,11 @@ import MotherScreen from "../class/MotherScreen";
 class AccueilScreen extends MotherScreen{
 
     override render(){
-        return `<nav class="navbar navbar-expand-lg bg-parchemin">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="./src/assets/img/icon-park-outline_socks.svg"
-                    style="width:8vw; height:8vh;"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active text-rouge-velours  fs-3" aria-current="page" href="#">Chaussette</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="connect">
-                    <div class="">
-                        <div class="d-flex pb-2">
-                            <div class="d-flex align-items-center">
-                                <img src="./src/assets/img/memory_email.svg" style="width:5vw; height:4vh;">
-                                <input class="form-control" type="text" placeholder="Identifiant" aria-label="Id">
-                            </div>
-                            <div class="d-flex align-items-center">
-                                <img class="mx-2" src="./src/assets/img/memory_key.svg" style="width:3vw; height:4vh;">
-                                <input class="form-control" type="password" placeholder="Mot de passe" aria-label="Mdp">
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-end pe-1">
-                            <button class="btn btn-sm btn-rouge-velours mx-2" type="submit">Connection</button>
-                            <button class="btn btn-sm btn-rouge-velours" type="submit">Chaussette ?</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </nav>
+        return `
+        <div><signin-header-component></div>
 
-    <div class = "bg-image d-flex align-items-center justify-content-center pb-5">
-        <div class="container row">
+        <div class = "bg-image d-flex align-items-center justify-content-center pb-5">
+            <div class="container row">
                 <div class=" col-12 container bg-white bg-opacity-75 text-rouge-velours text-center border border-4 border-rouge-velours fs-4 py-5 px-4">
                     Bonjour aventurier en quête de gloire (ou de simple survie) dans l'univers rocambolesque des Donjons de Naheulbeuk ! 🌟 <br />
                     Bienvenue sur notre site web, le sanctuaire ultime pour les maîtres du jeu et les héros en herbe qui cherchent à plonger <br />
@@ -54,11 +20,10 @@ class AccueilScreen extends MotherScreen{
                     Préparez vos dés, affûtez vos haches et en avant pour des aventures mémorables où le ridicule ne tue pas… mais les pièges si.<br />
                     À bientôt dans les couloirs sombres et les tavernes bruyantes de Naheulbeuk ! 🗡️🎲🍺<br />
                 </div>
+            </div>
         </div>
-    </div>
 
-    <footer-component>
-       `;
+        <div><footer-component></div>`;
     }
 }
 customElements.define("accueil-screen", AccueilScreen);
